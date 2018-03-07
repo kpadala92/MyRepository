4d5ae7f8baa1abd29d12f90a23810d91bab4ad40
Kishore.Padala <Kishore.Padala@TES-KISHOREP.as.domcorp.zetainteractive.com>
2018-03-07 15:41:07 +0530

    First commit

---
+++ ahmis/src/main/java/com/ahmis/main/AhmisApplication.java
@@ -0,0 +1,12 @@
+package com.ahmis.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhmisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhmisApplication.class, args);
+	}
+}
---
+++ ahmis/src/test/java/com/ahmis/main/AhmisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ahmis.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AhmisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
